c18e5536d4ddb0d0eaf44535773039a363c14b5c
sarthak3154 <sarthak.arora3154@gmail.com>
2019-06-21 12:51:55 -0400

    Add Two Numbers solution with BigInteger | all test cases passed

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,11 +1,13 @@
+import java.math.BigInteger;
+
 public class AddTwoNumbers {
 
-    private ListNode createLinkedList(long num) {
-        ListNode rootNode = new ListNode((int) (num % 10));
+    private ListNode createLinkedList(BigInteger num) {
+        ListNode rootNode = new ListNode(num.mod(BigInteger.valueOf(10)).intValue());
         rootNode.next = null;
         ListNode prevNode = rootNode;
-        while ((num = num / 10) != 0) {
-            ListNode listNode = new ListNode((int) (num % 10));
+        while (!(num = num.divide(BigInteger.valueOf(10))).equals(BigInteger.ZERO)) {
+            ListNode listNode = new ListNode(num.mod(BigInteger.valueOf(10)).intValue());
             listNode.next = null;
             prevNode.next = listNode;
             prevNode = listNode;
@@ -15,19 +17,20 @@ public class AddTwoNumbers {
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        long num1 = 0, num2 = 0, i = 0;
+        BigInteger num1 = BigInteger.ZERO, num2 = BigInteger.ZERO;
+        int i = 0;
         while (l1 != null) {
-            num1 += l1.val * (long) Math.pow(10, i++);
+            num1 = num1.add(BigInteger.valueOf((long) 10).pow(i++).multiply(BigInteger.valueOf((long) l1.val)));
             l1 = l1.next;
         }
 
         i = 0;
         while (l2 != null) {
-            num2 += l2.val * (long) Math.pow(10, i++);
+            num2 = num2.add(BigInteger.valueOf((long) 10).pow(i++).multiply(BigInteger.valueOf((long) l2.val)));
             l2 = l2.next;
         }
 
-        return createLinkedList(num1 + num2);
+        return createLinkedList(num1.add(num2));
     }
 
     /**
