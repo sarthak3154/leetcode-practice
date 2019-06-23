1ccbc9e7f864df309b972a1acb4b1c820bedef83
sarthak3154 <sarthak.arora3154@gmail.com>
2019-06-22 23:45:58 -0400

    inorder tree traversal solution

---
+++ src/trees/InorderTraversal.java
@@ -0,0 +1,58 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
+
+public class InorderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        TreeNode current = root;
+        List<Integer> inorderList = new ArrayList<>();
+        if (current == null) {
+            return inorderList;
+        }
+
+        HashSet<TreeNode> visited = new HashSet<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(current);
+
+        while (!stack.isEmpty()) {
+            current = stack.pop();
+            if (!visited.contains(current)) {
+                if (current.left == null && current.right == null) {
+                    inorderList.add(current.val);
+                } else {
+                    if (current.right != null) {
+                        stack.push(current.right);
+                    }
+                    stack.push(current);
+                    if (current.left != null) {
+                        stack.push(current.left);
+                    }
+                }
+                visited.add(current);
+            } else {
+                inorderList.add(current.val);
+            }
+        }
+
+        return inorderList;
+    }
+
+    public static void main(String[] args) {
+        // TODO call the inorderTraversal method
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
